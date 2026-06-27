60a1ae7ccfa563a9c12987b1808c9ff14ea15824
nkprannesh12 <nkprannesh12@gmail.com>
2026-06-28 01:00:32 +0530

    Completed Module 5 - Spring Core and Maven hands-on exercises

---
+++ Module-5-Spring-Core-Maven/library-management/src/main/java/com/library/BookService.java
@@ -0,0 +1,21 @@
+package com.library;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        System.out.println("Book Service Called");
+        bookRepository.displayBook();
+    }
+}
